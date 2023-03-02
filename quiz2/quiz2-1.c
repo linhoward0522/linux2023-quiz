da51fdc6cad80924ac05bb99be89d7562bc337ae
linhoward0522 <linhoward71@gmail.com>
2023-03-02 15:14:51 +0800

    Add new condition for quiz1
    Because we need to find the closest value that is a power of 2 or greater.

--- quiz2/quiz2-1.c
+++ quiz2/quiz2-1.c
@@ -1,8 +1,25 @@
 uint64_t next_pow2(uint64_t x)
 {
-    if (x == 0)
+    x |= x >> 1;
+    x |= x >> 1;
+    x |= x >> 1;
+    x |= x >> 1;
+    x |= x >> 1;
+    x |= x >> 1;
+    x |= x >> 1;
+    x |= x >> 8;
+    x |= x >> 16;
+    x |= x >> 32;
+    return x + 1;
+}    
+
+
+uint64_t next_pow2_(uint64_t x)
+{
+    if (!x)
         return 1;
+    if (!(x & (x - 1)))
+        return x;
     int clz = __builtin_clzl (x);
     return (1 << (64 - clz));
 }
-
